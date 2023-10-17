bccbf97e7a61bde3c6654762d85b4b88f5153977
Stephen <stephensouness@outlook.com>
2023-10-17 18:12:01 +0100

    all functionality works in the browser, just needs quite a bit of refactoring!

--- script.js
+++ script.js
@@ -51,16 +51,42 @@ myLibrary.forEach((books) => {
   author.innerText = `Author: ${books.author}`;
   pages.innerText = `${books.pages} pages`;
   haveRead.innerText = books.read;
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
+  const changeReadButton = document.createElement("button");
+  changeReadButton.innerText = "Read?";
 
   bookCard.appendChild(title);
   bookCard.appendChild(author);
   bookCard.appendChild(pages);
   bookCard.appendChild(haveRead);
+  bookCard.appendChild(deleteButton);
+  bookCard.appendChild(changeReadButton);
   container.appendChild(bookCard);
+
+  deleteButton.addEventListener("click", () => {
+    // this works visually but I think I'm just hiding the card as opposed to deleting it
+    title.innerText = "";
+    author.innerText = "";
+    pages.innerText = "";
+    haveRead.innerText = "";
+    bookCard.style.display = "none";
+    console.log(myLibrary);
+  });
+
+  let active = false;
+
+  changeReadButton.addEventListener("click", () => {
+    active = !active;
+    if (active) {
+      haveRead.innerText = "notRead";
+    } else {
+      haveRead.innerText = "Read!!";
+    }
+  });
 });
 
 newBookButton.addEventListener("click", () => {
-  console.log("I'm a little Jeevy");
   bookForm.style.display = "block";
 });
 
@@ -83,13 +109,42 @@ addButton.addEventListener("click", (event) => {
     (author.innerText = `Author: ${formAuthor.value}`);
   pages.innerText = `${formPages.value} pages`;
   haveRead.innerText = formHaveRead.value;
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
+
+  const changeReadButton = document.createElement("button");
+  changeReadButton.innerText = "Read?";
+
   bookCard.appendChild(title);
   bookCard.appendChild(author);
   bookCard.appendChild(pages);
   bookCard.appendChild(haveRead);
+  bookCard.appendChild(deleteButton);
+  bookCard.appendChild(changeReadButton);
   container.appendChild(bookCard);
 
   bookForm.style.display = "none";
+
+  deleteButton.addEventListener("click", () => {
+    // this works visually but I think I'm just hiding the card as opposed to deleting it
+    title.innerText = "";
+    author.innerText = "";
+    pages.innerText = "";
+    haveRead.innerText = "";
+    bookCard.style.display = "none";
+    console.log(myLibrary);
+  });
+
+  let active = false;
+
+  changeReadButton.addEventListener("click", () => {
+    active = !active;
+    if (active) {
+      haveRead.innerText = "notRead";
+    } else {
+      haveRead.innerText = "Read!!";
+    }
+  });
 });
 
 //Think this is still functioning as a submit button
