e0fb49e98045169b683ff31ff5f9e5f885f8290e
Stephen <stephensouness@outlook.com>
2023-10-18 14:42:26 +0100

    books now displayed in a table format in the dom

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-const container = document.querySelector(".container");
-
-const newBookButton = document.querySelector(".new-book-button");
-const cancelButton = document.querySelector(".cancel-book-button");
-const addButton = document.querySelector(".add-book-button");
-
 const bookForm = document.querySelector(".book-form");
 const formTitle = document.querySelector("#form-title");
 const formAuthor = document.querySelector("#form-author");
 const formPages = document.querySelector("#form-pages");
 const formHaveRead = document.querySelector("#form-have-read");
+
+const newBookButton = document.querySelector(".new-book-button");
+const addButton = document.querySelector(".add-book-button");
 
 const myLibrary = [];
 
@@ -17,93 +14,56 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function () {
-    return `${title} by ${author}, ${pages} pages, ${read}`;
-  };
 }
 
 function addBookToLibrary(book) {
-  return myLibrary.push(book);
+  myLibrary.push(book);
 }
 
-///////////////////
+const book1 = new Book("The Hobbit", "J.R.R Tolkien", 295, "read");
+const book2 = new Book("The Lord of the Rings", "J.R.R Tolkien", 1178, "read");
+const book3 = new Book("IT", "Stephen King", 1168, "read");
 
-// return myLibrary.forEach((books) => {
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+console.log(myLibrary);
 
-//     deleteButton.addEventListener("click", () => {
-//       // this works visually but I think I'm just hiding the card as opposed to deleting it
-//       title.innerText = "";
-//       author.innerText = "";
-//       pages.innerText = "";
-//       haveRead.innerText = "";
-//       bookCard.style.display = "none";
-//       console.log(myLibrary);
-//     });
+const bookTable = document.getElementById("book-table");
+const bookTableHeader = bookTable.querySelector("thead");
+const bookTableBody = bookTable.querySelector("tbody");
 
-//     let active = false;
+function displayBooks() {
+  bookTableHeader.innerHTML = "";
+  let tr = document.createElement("tr");
+  const headersData = Object.keys(myLibrary[0]);
+  headersData.forEach((key) => {
+    let th = document.createElement("th");
+    th.innerHTML = key;
+    tr.appendChild(th);
+  });
+  bookTableHeader.appendChild(tr);
+  bookTableBody.innerHTML = "";
+  for (let i = 0; i < myLibrary.length; i++) {
+    let tr = document.createElement("tr");
+    headersData.forEach((key) => {
+      let td = document.createElement("td");
+      td.innerHTML = myLibrary[i][key];
+      tr.appendChild(td);
+    });
+    bookTableBody.appendChild(tr);
+  }
+}
 
-//     changeReadButton.addEventListener("click", () => {
-//       active = !active;
-//       if (active) {
-//         haveRead.innerText = "notRead";
-//       } else {
-//         haveRead.innerText = "Read!!";
-//       }
-//     });
-//   });
-
-///////////////////
-
-addButton.addEventListener("click", (event) => {
-  event.preventDefault();
-  const bookx = new Book(
-    formTitle.value,
-    formAuthor.value,
-    formPages.value,
-    formHaveRead.value
-  );
-  addBookToLibrary(bookx);
-  const bookCard = document.createElement("div");
-  bookCard.className = "book-card";
-  const title = document.createElement("h3");
-  const author = document.createElement("p");
-  const pages = document.createElement("p");
-  const haveRead = document.createElement("p");
-  title.innerText = bookx.title;
-  author.innerText = `Author: ${bookx.author}`;
-  pages.innerText = `${bookx.pages} pages`;
-  haveRead.innerText = bookx.read;
-  const deleteButton = document.createElement("button");
-  deleteButton.innerText = "Delete";
-  const changeReadButton = document.createElement("button");
-  changeReadButton.innerText = "Read?";
-
-  bookCard.appendChild(title);
-  bookCard.appendChild(author);
-  bookCard.appendChild(pages);
-  bookCard.appendChild(haveRead);
-  bookCard.appendChild(deleteButton);
-  bookCard.appendChild(changeReadButton);
-  container.appendChild(bookCard);
-  console.log(myLibrary);
-});
+displayBooks();
 
 newBookButton.addEventListener("click", () => {
   bookForm.style.display = "block";
 });
 
-//Think this is still functioning as a submit button
-cancelButton.addEventListener("click", () => {
-  bookForm.style.display = "none";
+addButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  const bookx = new Book(formTitle.value, formAuthor.value, formPages.value, formHaveRead.value);
+  addBookToLibrary(bookx);
+  displayBooks();
 });
-
-//   let active = false;
-
-//   changeReadButton.addEventListener("click", () => {
-//     active = !active;
-//     if (active) {
-//       haveRead.innerText = "notRead";
-//     } else {
-//       haveRead.innerText = "Read!!";
-//     }
-//   });
