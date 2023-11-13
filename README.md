4fb0a549fd7f904adbec8384d09e84a0985b77cb
Stephen <stephensouness@outlook.com>
2023-11-13 13:55:20 +0000

    refactored function constructor to class

--- README.md
+++ README.md
@@ -3,5 +3,3 @@ README
 https://souste.github.io/library/
 
 A simple Object Orientated Programming App for creating and storing simple book objects on the screen
-
-a
--- script.js
+++ script.js
@@ -13,11 +13,13 @@ const bookTableBody = bookTable.querySelector("tbody");
 
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
 function addBookToLibrary(book) {
